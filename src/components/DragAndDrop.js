71062931f874ac341c49c78986eab22f9b5c2b2a
David Smith <david@dividia.net>
2019-12-20 16:13:58 -0600

    added some css for onDrag events

--- src/components/DragAndDrop.js
+++ src/components/DragAndDrop.js
@@ -12,15 +12,27 @@ class DragAndDrop extends React.Component {
         {name: 'Block 3', category: 'new', position: 0, bgcolor: 'skyblue'},
         {name: 'Block 4', category: 'new', position: 0, bgcolor: 'lightgreen'},
         {name: 'Block 5', category: 'new', position: 0, bgcolor: 'white'}
-      ]
+      ],
     }
   }
 
-  onDragOver = e => {
+  onDragOver = (e, cat) => {
     e.preventDefault();
   }
 
+  onDragLeave = (e, cat) => {
+    e.preventDefault();
+  }
+
+  onDragEnd = ( e, cat) => {
+    e.preventDefault();
+    e.target.style.opacity = '1';
+    e.target.style.transform = 'scale(1)';
+  }
+
   onDragStart = (e, obj) => {
+    e.target.style.opacity = '0.3';
+    e.target.style.transform = 'scale(.8)';
     // convert the object into a string
     let o = JSON.stringify(obj);
     console.log(o)
@@ -29,24 +41,25 @@ class DragAndDrop extends React.Component {
   }
 
   onDrop = (e, cat, pos) => {
+    e.target.style.opacity = '1';
+    e.target.style.transform = 'scale(1)';
     // pull the dragged object dataTransfer and convert back to object literal from string
     let obj = JSON.parse(e.dataTransfer.getData("obj"));
-    // if this object is comin from somewhere other than complet it should have a position value of 0
+    // if this object is coming from somewhere other than complete it should have a position value of 0
     if (obj.category !== 'complete') {
       obj.position = 0;
     }
     let spotsTaken = [];
-    // mmake a list of spots that currently have a task object 
+    // make a list of spots that currently have a task object 
     this.state.tasks.forEach( t => {
       if ( t.category === cat && t.position !== 0 && t.position !== obj.position ) {
        spotsTaken.push(t.position);
       }
     });
-    console.log(spotsTaken)
     let tasks = this.state.tasks.filter( task => {
       // see if we have an existing task in that position
       if( task.position === pos) {
-        // what spots are available
+        // what spots are available?
         if ( spotsTaken.indexOf(1) < 0 ) {
           task.position = 1
         } else if ( spotsTaken.indexOf(2) < 0 ) {
@@ -85,9 +98,10 @@ class DragAndDrop extends React.Component {
         // we are setting the entire object to the key so it can be accessed later
         <div  key={JSON.stringify(t)}
               onDragStart={ e => this.onDragStart(e, t )}
+              onDragEnd={ e => this.onDragEnd(e, t.category )}
               draggable
               className="draggable"
-              style={{backgroundColor: t.bgcolor}} >
+              style={{ backgroundColor: t.bgcolor, opacity: '1', transition: '1' }} >
           <p className="task-title">{t.name}</p>
         </div>
       )
@@ -101,7 +115,7 @@ class DragAndDrop extends React.Component {
 
 
         <div  className="new"
-              onDragOver={ e => this.onDragOver(e) } 
+              onDragOver={ e => this.onDragOver(e, 'new') } 
               onDrop={ e => this.onDrop(e, "new", 0) } >
           <span className="task-header">Tasks</span>
 
@@ -110,7 +124,7 @@ class DragAndDrop extends React.Component {
         </div>
 
         <div  className="wip"
-              onDragOver={ e => this.onDragOver(e) } 
+              onDragOver={ e => this.onDragOver(e, 'wip') } 
               onDrop={ e => this.onDrop(e, "wip", 0) } >
           <span className="task-header">In Progress</span>
 
@@ -118,21 +132,23 @@ class DragAndDrop extends React.Component {
 
         </div>
         
-        <div className="droppable" >
+        <div className="droppable" style={{ border: 'none'}}>
 
           <span className="task-header">Completed</span>
 
           <div  style={{ height: 145, width: 200, borderRadius: 10, border: '4px dashed grey', marginRight: 'auto', marginLeft: 'auto', marginTop: 20, position: 'relative' }}
-                onDragOver={ e => this.onDragOver(e) } 
+                onDragOver={ e => this.onDragOver(e, 'complete') } 
+                onDragLeave={ e => this.onDragLeave(e, 'complete') }
                 onDrop={ e => this.onDrop(e, "complete", 1) } >
                 {tasks.complete.filter( t => JSON.parse(t.key).position === 1).length ? 
                   tasks.complete.filter( t => JSON.parse(t.key).position === 1) :
-                  <span style={{ fontSize: 50, fontWeight: 'bold', position: 'absolute', top: 40, left: 0, right: 0, margin: 'auto' }}>1</span> 
+                  <span style={{ border: 'none',fontSize: 50, fontWeight: 'bold', position: 'absolute', top: 40, left: 0, right: 0, margin: 'auto' }}>1</span> 
                 }
           </div>
 
           <div  style={{ height: 145, width: 200, borderRadius: 10, border: '4px dashed grey', marginRight: 'auto', marginLeft: 'auto', marginTop: 20, position: 'relative'  }}
-                onDragOver={ e => this.onDragOver(e) } 
+                onDragOver={ e => this.onDragOver(e, 'complete') } 
+                onDragLeave={ e => this.onDragLeave(e, 'complete') }
                 onDrop={ e => this.onDrop(e, "complete", 2) } >
                 {tasks.complete.filter( t => JSON.parse(t.key).position === 2).length ?
                 tasks.complete.filter( t => JSON.parse(t.key).position === 2) :
@@ -141,7 +157,8 @@ class DragAndDrop extends React.Component {
           </div>
 
           <div  style={{ height: 145, width: 200, borderRadius: 10, border: '4px dashed grey', marginRight: 'auto', marginLeft: 'auto', marginTop: 20, position: 'relative'  }}
-                onDragOver={ e => this.onDragOver(e) } 
+                onDragOver={ e => this.onDragOver(e, 'complete') } 
+                onDragLeave={ e => this.onDragLeave(e, 'complete') }
                 onDrop={ e => this.onDrop(e, "complete", 3) } >
                 {tasks.complete.filter( t => JSON.parse(t.key).position === 3).length ?
                   tasks.complete.filter( t => JSON.parse(t.key).position === 3) :
@@ -150,7 +167,8 @@ class DragAndDrop extends React.Component {
           </div>
 
           <div  style={{ height: 145, width: 200, borderRadius: 10, border: '4px dashed grey', marginRight: 'auto', marginLeft: 'auto', marginTop: 20, position: 'relative'  }}
-                onDragOver={ e => this.onDragOver(e) } 
+                onDragOver={ e => this.onDragOver(e, 'complete') } 
+                onDragLeave={ e => this.onDragLeave(e, 'complete') }
                 onDrop={ e => this.onDrop(e, "complete", 4) } >
                 {tasks.complete.filter( t => JSON.parse(t.key).position === 4).length ? 
                   tasks.complete.filter( t => JSON.parse(t.key).position === 4) :
@@ -159,7 +177,8 @@ class DragAndDrop extends React.Component {
           </div>
 
           <div  style={{ height: 145, width: 200, borderRadius: 10, border: '4px dashed grey', marginRight: 'auto', marginLeft: 'auto', marginTop: 20, position: 'relative'  }}
-                onDragOver={ e => this.onDragOver(e) } 
+                onDragOver={ e => this.onDragOver(e, 'complete') } 
+                onDragLeave={ e => this.onDragLeave(e, 'complete') }
                 onDrop={ e => this.onDrop(e, "complete", 5) } >
                 {tasks.complete.filter( t => JSON.parse(t.key).position === 5).length ? 
                   tasks.complete.filter( t => JSON.parse(t.key).position === 5) :
